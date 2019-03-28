b110768f0b4af42a0e4fbee4fe217ae8416a14a9
daniel gomez calderon <gomezcalderondaniel@gmail.com>
2019-03-28 01:38:21 -0600

    Using map()

--- src/App.js
+++ src/App.js
@@ -97,14 +97,15 @@ class Filter extends Component {
 
 class Playlist extends Component{
   render () {
+    let playlist = this.props.playlist;
     return(
       <div style={{...defaultStyle, width: "25%", display: "inline-block"}}> 
         <img/>
-        <h3>Playlist Name</h3>
+        <h3>{playlist.name}</h3>
         <ul>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {playlist.songs.map(song =>
+            <li>{song.name}</li>
+            )}
         </ul>
       </div>
     );
@@ -135,10 +136,11 @@ class App extends Component {
           <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
           <HoursCounter playlists={this.state.serverData.user.playlists}/>
           <Filter/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
-          <Playlist/>
+          {
+            this.state.serverData.user.playlists.map(playlist =>
+            <Playlist playlist={playlist}/>
+            )
+          }
         </div> : <h1 style={defaultStyle}>Loading...</h1>
         }  
       </div>
